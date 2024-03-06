96492f3d25f8285655ba5c02ccd3a2dec85f7e2d
Sarah Egan <sarah.egan@formidable.com>
2024-03-06 14:40:46 -0700

    basic query set up

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,3 +1,30 @@
-export const MovieCard = () => {
-  return <>Movie Card</>
+import { FragmentOf, graphql, readFragment } from "@/fuse"
+
+export const MovieFields = graphql(`
+  fragment MovieCard_MovieFields on Movie {
+    Title
+    Year
+    Poster
+  }
+`)
+
+export const MovieCard = (props: { movie: FragmentOf<typeof MovieFields> }) => {
+  const { Title, Poster, Year } = readFragment(MovieFields, props.movie)
+
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl mb-11">
+      <figure>
+        <img src={Poster} alt="Movie Poster" className="pt-7" />
+      </figure>
+      <div className="card-body">
+        <div className="card-actions justify-around">
+          <div>
+            <h2 className="text-lg">{Title}</h2>
+            <p>{Year}</p>
+          </div>
+          <button className="btn btn-primary">Find out more</button>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,40 +1,59 @@
-import { node } from "fuse"
+import { node, addQueryFields } from "fuse"
 
-type MovieSource = {
+interface Movie {
   Title: string
   Year: string
   imdbID: string
   Type: string
+  Poster: string
 }
 
 // "Nodes" are the core abstraction of Fuse. Each node represents
 // a resource/entity with multiple fields and has to define two things:
 // 1. load(): How to fetch from the underlying data source
 // 2. fields: What fields should be exposed and added for clients
-export const MovieNode = node<MovieSource>({
+export const MovieNode = node<Movie>({
   name: "Movie",
-  load: async searchTerm => getMovieData("tenet"),
+  key: "imdbID",
+  load: async ids => getMovieData(ids),
   fields: t => ({
     Title: t.exposeString("Title"),
     Year: t.exposeString("Year"),
     imdbID: t.exposeString("imdbID"),
     Type: t.exposeString("Type"),
+    Poster: t.exposeString("Poster"),
   }),
 })
 
-// async function getWeatherData(ids: string[]): Promise<UserSource[]> {
-//   return ids.map((id) => ({
-//     id,
-//     name: `Peter #${id}`,
-//     avatar_url: `https://i.pravatar.cc/300?u=${id}`,
-//   }))
-// }
-
-async function getMovieData(searchTerm: string): Promise<MovieSource[]> {
-  const response = await fetch(
-    `http://www.omdbapi.com/?apikey=d25c02a3&s=${searchTerm}`
+async function getMovieData(ids: string[]): Promise<Movie[]> {
+  const movies = await Promise.allSettled(
+    ids.map(id =>
+      fetch(`http://www.omdbapi.com/?apikey=d25c02a3&i=${id}`).then(x =>
+        x.json()
+      )
+    )
   )
-  const data = await response.json()
-  console.log(data)
-  return data
+
+  return await Promise.all(
+    movies.map(movie =>
+      movie.status === "fulfilled" ? movie.value : new Error(movie.reason)
+    )
+  )
 }
+
+addQueryFields(t => ({
+  movies: t.list({
+    type: MovieNode,
+    resolve: async () => {
+      const movies = await getMovieData([
+        "tt6723592",
+        "tt0816692",
+        "tt1375666",
+        "tt2935510",
+        "tt6710474",
+        "tt0468569",
+      ])
+      return { nodes: movies, totalCount: movies.length }
+    },
+  }),
+}))
